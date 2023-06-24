cb40ac0eadf9cd09150e545aa65ea6de15736942
Shishir Rijal <ccrrizal0892@gmail.com>
2023-06-25 03:04:34 +0545

    added readmore package and used to write description section

--- lib/features/shop/presentation/product_detail/product_detail_view.dart
+++ lib/features/shop/presentation/product_detail/product_detail_view.dart
@@ -1,5 +1,6 @@
 import 'package:clothing_store/core/resources/resources.dart';
 import 'package:flutter/material.dart';
+import 'package:readmore/readmore.dart';
 
 class ProductDetailView extends StatelessWidget {
   const ProductDetailView({super.key});
@@ -48,34 +49,53 @@ class ProductDetailView extends StatelessWidget {
             ),
             const SizedBox(height: 10),
             // Ratings and reviews
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                const Icon(Icons.star, color: Colors.amber, size: 24),
-                const SizedBox(width: 5),
-                Text(
-                  "4.8",
-                  style: getBoldTextStyle(size: 18),
-                ),
-                const SizedBox(width: 5),
-                Text(
-                  "(335)",
-                  style:
-                      getRegularTextStyle(size: 18, color: ColorManager.grey),
-                ),
-                const SizedBox(width: 10),
-                const Icon(Icons.circle, size: 7, color: ColorManager.grey),
-                const SizedBox(width: 10),
-                Text(
-                  "(124 Reviews)",
-                  style:
-                      getRegularTextStyle(size: 18, color: ColorManager.grey),
-                ),
-              ],
+            const RatingAndReview(),
+            const SizedBox(height: 20),
+            // * Description
+            ReadMoreText(
+              "This is a traditional blue kurthi with a beautiful design. It is made of cotton and is very comfortable to wear. It is available in all sizes. This is a traditional blue kurthi with a beautiful design. It is made of cotton and is very comfortable to wear. It is available in all sizes. This is a traditional blue kurthi with a beautiful design. It is made of cotton and is very comfortable to wear. It is available in all sizes and colors.",
+              trimLength: 160,
+              trimExpandedText: ' Read less',
+              trimCollapsedText: 'Read more',
+              trimMode: TrimMode.Length,
+              style: getRegularTextStyle(size: 18, color: ColorManager.grey),
             ),
           ]),
         ),
       ),
+    );
+  }
+}
+
+class RatingAndReview extends StatelessWidget {
+  const RatingAndReview({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        const Icon(Icons.star, color: Colors.amber, size: 24),
+        const SizedBox(width: 5),
+        Text(
+          "4.8",
+          style: getBoldTextStyle(size: 18),
+        ),
+        const SizedBox(width: 5),
+        Text(
+          "(335)",
+          style: getRegularTextStyle(size: 18, color: ColorManager.grey),
+        ),
+        const SizedBox(width: 10),
+        const Icon(Icons.circle, size: 7, color: ColorManager.grey),
+        const SizedBox(width: 10),
+        Text(
+          "(124 Reviews)",
+          style: getRegularTextStyle(size: 18, color: ColorManager.grey),
+        ),
+      ],
     );
   }
 }
