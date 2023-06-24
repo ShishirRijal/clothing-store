273ee7d8afc6e1c91c918ed44dccb28bea5a495f
Shishir Rijal <ccrrizal0892@gmail.com>
2023-06-25 02:58:21 +0545

    added rating and reviews

--- lib/features/shop/presentation/product_detail/product_detail_view.dart
+++ lib/features/shop/presentation/product_detail/product_detail_view.dart
@@ -26,7 +26,9 @@ class ProductDetailView extends StatelessWidget {
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.all(20),
-          child: Column(children: [
+          child:
+              Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+            // * Product Image
             SizedBox(
               height: MediaQuery.of(context).size.height * 0.4,
               width: double.infinity,
@@ -37,7 +39,40 @@ class ProductDetailView extends StatelessWidget {
                   fit: BoxFit.fill,
                 ),
               ),
-            )
+            ),
+            const SizedBox(height: 20),
+            //* Product Name
+            Text(
+              "Traditional Blue Kurthi",
+              style: getSemiBoldTextStyle(size: 22),
+            ),
+            const SizedBox(height: 10),
+            // Ratings and reviews
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const Icon(Icons.star, color: Colors.amber, size: 24),
+                const SizedBox(width: 5),
+                Text(
+                  "4.8",
+                  style: getBoldTextStyle(size: 18),
+                ),
+                const SizedBox(width: 5),
+                Text(
+                  "(335)",
+                  style:
+                      getRegularTextStyle(size: 18, color: ColorManager.grey),
+                ),
+                const SizedBox(width: 10),
+                const Icon(Icons.circle, size: 7, color: ColorManager.grey),
+                const SizedBox(width: 10),
+                Text(
+                  "(124 Reviews)",
+                  style:
+                      getRegularTextStyle(size: 18, color: ColorManager.grey),
+                ),
+              ],
+            ),
           ]),
         ),
       ),
