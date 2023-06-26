c3e701033090f03b8ac5f5ffced8e0cb25a5aad0
Shishir Rijal <ccrrizal0892@gmail.com>
2023-06-26 18:35:22 +0545

    replaced product image with image carousel and added dot indicators

--- lib/features/shop/presentation/product_detail/product_detail_view.dart
+++ lib/features/shop/presentation/product_detail/product_detail_view.dart
@@ -1,9 +1,37 @@
 import 'package:clothing_store/core/resources/resources.dart';
 import 'package:flutter/material.dart';
+import 'package:infinite_carousel/infinite_carousel.dart';
 import 'package:readmore/readmore.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
-class ProductDetailView extends StatelessWidget {
+class ProductDetailView extends StatefulWidget {
   const ProductDetailView({super.key});
+
+  @override
+  State<ProductDetailView> createState() => _ProductDetailViewState();
+}
+
+class _ProductDetailViewState extends State<ProductDetailView> {
+  final images = [
+    'https://shaposh.pk/34438-large_default/formal-2187-af-nt.jpg',
+    'https://i.pinimg.com/736x/94/68/d7/9468d7bc427062875b080ccadb79aff1.jpg',
+    'https://www.kessa.com/wp-content/uploads/2020/08/kessa-ws564-floral-labyrinth-short-kurti-closeup-hd.jpg',
+  ];
+  final controller = PageController();
+
+  @override
+  void initState() {
+    super.initState();
+    controller.addListener(() {
+      setState(() {});
+    });
+
+    @override
+    void dispose() {
+      controller.dispose();
+      super.dispose();
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -30,17 +58,7 @@ class ProductDetailView extends StatelessWidget {
           child:
               Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
             // * Product Image
-            SizedBox(
-              height: MediaQuery.of(context).size.height * 0.4,
-              width: double.infinity,
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(15),
-                child: Image.network(
-                  'https://shaposh.pk/34438-large_default/formal-2187-af-nt.jpg',
-                  fit: BoxFit.fill,
-                ),
-              ),
-            ),
+            HeroImage(images: images, controller: controller),
             const SizedBox(height: 20),
             //* Product Name
             Text(
@@ -63,6 +81,49 @@ class ProductDetailView extends StatelessWidget {
           ]),
         ),
       ),
+    );
+  }
+}
+
+class HeroImage extends StatelessWidget {
+  const HeroImage({required this.images, required this.controller, super.key});
+  final List<String> images;
+  final PageController controller;
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        SizedBox(
+          height: MediaQuery.of(context).size.height * 0.4,
+          child: PageView.builder(
+            itemCount: images.length,
+            controller: controller,
+            itemBuilder: (context, index) => Container(
+              margin: const EdgeInsets.symmetric(horizontal: 10),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(15),
+                child: Image.network(
+                  images[index],
+                  fit: BoxFit.fill,
+                ),
+              ),
+            ),
+          ),
+        ),
+        const SizedBox(height: 20),
+        Center(
+          child: SmoothPageIndicator(
+            controller: controller, // PageController
+            count: images.length,
+            // forcing the indicator to use a specific direction
+            textDirection: TextDirection.ltr,
+            effect: const ExpandingDotsEffect(
+              expansionFactor: 2,
+              dotHeight: 15,
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
