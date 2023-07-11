9d031b907db5d25e9e3b509eb93b05598654538e
Shishir Rijal <ccrrizal0892@gmail.com>
2023-07-11 21:28:39 +0545

    changed the logic to add product color and refactored add product view and netorking

--- lib/core/app.dart
+++ lib/core/app.dart
@@ -1,7 +1,10 @@
 import 'package:clothing_store/core/theme.dart';
+import 'package:clothing_store/features/admin_panel/presentation/presentation.dart';
 import 'package:clothing_store/features/authentication/data/services/auth_services.dart';
 import 'package:clothing_store/features/cart/presentation/providers/cart.dart';
+import 'package:clothing_store/features/shop/domain/entities/product.dart';
 import 'package:clothing_store/features/shop/presentation/main/main_view.dart';
+import 'package:clothing_store/features/shop/presentation/product_detail/product_detail_view.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -25,7 +28,7 @@ class MyApp extends StatelessWidget {
       ],
       child: MaterialApp(
         theme: theme,
-        home: const MainView(),
+        home: AdminMainView(),
         // home: Builder(builder: (context) {
         //   return StreamProvider(
         //     create: (_) => context.read<AuthServices>().authStateChanges,
--- lib/core/extensions.dart
+++ lib/core/extensions.dart
@@ -1,1 +1,38 @@
-
+extension ColorValue on int {
+  String get valueColor {
+    switch (this) {
+      case 4294198070:
+        return 'Red';
+      case 4280391411:
+        return 'Blue';
+      case 4283215696:
+        return 'Green';
+      case 4294961979:
+        return 'Yellow';
+      case 4288423856:
+        return 'Purple';
+      case 4294940672:
+        return 'Orange';
+      case 4293467747:
+        return 'Pink';
+      case 4286141768:
+        return 'Brown';
+      case 4278190080:
+        return 'Black';
+      case 4294967295:
+        return 'White';
+      case 4288585374:
+        return 'Grey';
+      case 4278238420:
+        return 'Cyan';
+      case 4278228616:
+        return 'Teal';
+      case 4282339765:
+        return 'Indigo';
+      case 4291681337:
+        return 'Lime';
+      default:
+        return 'Unknown';
+    }
+  }
+}
